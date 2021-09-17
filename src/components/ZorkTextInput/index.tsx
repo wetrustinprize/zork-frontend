e1b9c8f42802eff4c3a6ee76f0feecbeb25239d6
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-09-17 12:15:16 -0300

    inputs now have names

--- src/components/ZorkTextInput/index.tsx
+++ src/components/ZorkTextInput/index.tsx
@@ -1,15 +1,17 @@
-import { InputHTMLAttributes, ReactElement } from "react";
+import { ReactElement } from "react";
 
 import styles from "./style.module.scss";
 
 interface ZorkTextInputProps {
   icon: ReactElement;
   type: string;
+  name: string;
   placeholder?: string;
 }
 
 const ZorkTextInput: React.FC<ZorkTextInputProps> = ({
   icon,
+  name,
   type = "text",
   placeholder = "",
 }) => {
@@ -17,7 +19,7 @@ const ZorkTextInput: React.FC<ZorkTextInputProps> = ({
     <div className={styles.zInputText}>
       {icon}
       <div />
-      <input type={type} placeholder={placeholder} />
+      <input type={type} placeholder={placeholder} name={name} />
     </div>
   );
 };
