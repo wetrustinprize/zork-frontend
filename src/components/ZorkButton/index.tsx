f71af9be809847fd0467595309d3c8c31ffa647b
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-09-17 12:14:44 -0300

    now a zork button can be a form submit

--- src/components/ZorkButton/index.tsx
+++ src/components/ZorkButton/index.tsx
@@ -2,10 +2,15 @@ import styles from "./style.module.scss";
 
 interface ZorkButtonProps {
   text: string;
+  isSubmit?: boolean;
 }
 
-const ZorkButton: React.FC<ZorkButtonProps> = ({ text }) => {
-  return <button className={styles.zbutton}>{text}</button>;
+const ZorkButton: React.FC<ZorkButtonProps> = ({ text, isSubmit = false }) => {
+  return isSubmit ? (
+    <input type="submit" className={styles.zbutton} value={text} />
+  ) : (
+    <button className={styles.zbutton}>{text}</button>
+  );
 };
 
 export default ZorkButton;
