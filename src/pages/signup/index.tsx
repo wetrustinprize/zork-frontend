f09a1ad7a0c46b334fb8d5d48396402b0143c724
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-09-20 12:15:24 -0300

    "functional" signup

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -8,8 +8,28 @@ import { BsLock, BsPerson } from "react-icons/bs";
 
 import styles from "./signup.module.scss";
 
+import { useState } from "react";
+import { registerUser } from "src/Services/User/registerUser";
+
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Signup: React.FC = () => {
-  const handleSingupSubmit = async () => {};
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSingupSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = await registerUser({ fullname, email, password });
+
+    if (data.error) {
+      toast.error(data.error);
+    } else {
+      toast.success("Usuario criado!");
+    }
+  };
 
   return (
     <>
@@ -25,22 +45,33 @@ const Signup: React.FC = () => {
             placeholder="Your full name"
             icon={<BsPerson size="20px" />}
             name="fullName"
+            onChange={(e) => {
+              setFullname(e.target.value);
+            }}
           />
           <ZorkTextInput
             type="text"
             placeholder="Your email"
             icon={<AiOutlineMail size="20px" />}
             name="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
           <ZorkTextInput
             type="password"
             placeholder="Your password"
             icon={<BsLock size="20px" />}
             name="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <ZorkButton text="Register" isSubmit />
         </form>
       </main>
+
+      <ToastContainer position="top-center" />
     </>
   );
 };
