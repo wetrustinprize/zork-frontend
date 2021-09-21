6fe0c723f0b802f2308200cd0f0b6ee2d21100a5
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-09-21 12:20:48 -0300

    transaction page

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,38 @@
 import ZorkLayout from "@components/ZorkLayout";
 import ZorkSidebar from "@components/ZorkSidebar";
+import ZorkTransaction from "@components/ZorkTransaction";
+
 import { NextPageWithLayout } from "@pages/utils";
 
 import type { ReactElement } from "react";
 
+import style from "./style.module.scss";
+
 const Transactions: NextPageWithLayout = () => {
-  return <></>;
+  return (
+    <div className={style.container}>
+      <h1>Latest Zork transactions</h1>
+
+      <main>
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+        <ZorkTransaction />
+      </main>
+    </div>
+  );
 };
 
 Transactions.getLayout = (page: ReactElement) => {
