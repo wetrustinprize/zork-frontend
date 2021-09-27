f9d61e44b15c0d2622beedf2b022417264428080
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-09-27 15:56:30 -0300

    onclick

--- src/components/ZorkButton/index.tsx
+++ src/components/ZorkButton/index.tsx
@@ -1,15 +1,23 @@
+import { MouseEventHandler } from "hoist-non-react-statics/node_modules/@types/react";
 import styles from "./style.module.scss";
 
 interface ZorkButtonProps {
   text: string;
   isSubmit?: boolean;
+  onClick?: () => void;
 }
 
-const ZorkButton: React.FC<ZorkButtonProps> = ({ text, isSubmit = false }) => {
+const ZorkButton: React.FC<ZorkButtonProps> = ({
+  text,
+  isSubmit = false,
+  onClick = () => {},
+}) => {
   return isSubmit ? (
     <input type="submit" className={styles.zbutton} value={text} />
   ) : (
-    <button className={styles.zbutton}>{text}</button>
+    <button className={styles.zbutton} onClick={onClick}>
+      {text}
+    </button>
   );
 };
 
