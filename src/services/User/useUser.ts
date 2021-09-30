5db38ef7b5735fdf3232b877b529abe780eb2a26
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-09-30 14:45:25 -0300

    now user information is stored in the cookie jar
    returns a reloadUser function, if neededed to update the user info

--- src/services/User/useUser.ts
+++ src/services/User/useUser.ts
@@ -9,6 +9,7 @@ import type { User } from "./utils";
 
 /**
  * React hook that give information about the authenticated user. Also returns the access token to make other requests.
+ * The Access Token and User information are stored in the cookie jar.
  * @param redirectTo The url to redirect if use is not authenticated, if set to undefined, it doesn't redirect to any page. Default: undefined.
  * @param redirectIfFound Should redirect if found user instead of if not found? Default: false.
  * @returns The access token and the User object
@@ -16,59 +17,70 @@ import type { User } from "./utils";
 const useUser = (
   redirectTo: string = "/login",
   redirectIfFound: boolean = false
-): { access_token: string; user: User } => {
-  const [cookie] = useCookies(["access_token"]);
-  const [user, setUser] = useState(undefined as User);
+): { access_token: string; user: User; reloadUser: () => void } => {
+  const [cookies, setCookies, removeCookies] = useCookies([
+    "access_token",
+    "user",
+  ]);
 
-  useEffect(() => {
-    // Needed a async function to execute axios
-    async function checkValidToken() {
-      try {
-        // Try to fetch User information
-        const response = await api.get("/user", {
-          headers: {
-            authorization: "Bearer " + cookie.access_token,
-          },
-        });
+  const reloadUser = () => {
+    removeCookies("user");
+  };
 
-        // Sets the new User information
-        setUser(response.data);
+  const getUser = async () => {
+    try {
+      // Try to fetch User information
+      const response = await api.get("/user", {
+        headers: {
+          authorization: "Bearer " + cookies.access_token,
+        },
+      });
 
-        // Check if should redirect if found User
-        if (redirectIfFound && redirectTo) {
+      // Sets the new User information
+      setCookies("user", response.data, {
+        path: "/",
+        sameSite: true,
+        maxAge: 0,
+      });
+
+      // Check if should redirect if found User
+      if (redirectIfFound && redirectTo) {
+        Router.push(redirectTo);
+      }
+    } catch (err) {
+      // Check if is a axios http error
+      if (!err.response) {
+        throw err;
+      }
+
+      // Check if is code 403
+      if (err.response.status == 403) {
+        // Check if should redirect if not found
+        if (!redirectIfFound && redirectTo) {
           Router.push(redirectTo);
-        }
-      } catch (err) {
-        // Check if is a axios http error
-        if (!err.response) {
-          throw err;
-        }
-
-        // Check if is code 403
-        if (err.response.status == 403) {
-          // Check if should redirect if not found
-          if (!redirectIfFound && redirectTo) {
-            Router.push(redirectTo);
-          } else {
-            setUser({} as User);
-          }
         } else {
-          throw err;
+          removeCookies("user");
         }
+      } else {
+        throw err;
       }
     }
+  };
 
+  useEffect(() => {
     // check if has access_token cookie
-    if (!cookie.access_token && !redirectIfFound && redirectTo) {
+    if (!cookies.access_token && !redirectIfFound && redirectTo) {
       Router.push(redirectTo);
       return;
     }
 
-    // check if is a valid token
-    checkValidToken();
-  }, [cookie.access_token]);
+    // check if has User in cookies
+    if (!cookies.user) {
+      getUser();
+    }
+  }, [cookies.access_token, cookies.user]);
 
-  return { access_token: cookie.access_token, user };
+  return { access_token: cookies.access_token, user: cookies.user, reloadUser };
 };
 
 export { useUser };
