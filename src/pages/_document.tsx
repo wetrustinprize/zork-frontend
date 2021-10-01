8d92b48d8b7f66c24134a5d863dbe4778bc9b2d5
ngustavin <gust4f3rn@gmail.com>
2021-10-02 01:18:39 +0400

    Section created with illustration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <meta charSet="utf-8" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           ></link>
         </Head>
