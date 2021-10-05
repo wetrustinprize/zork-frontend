e27855d44ceb7fe0e491b79f0a6df734c193438e
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-10-05 16:51:39 -0300

    react redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@ import { CookiesProvider } from "react-cookie";
 import "../styles/Global.scss";
 
 import { AppPropsWithLayout } from "@pages/utils";
+import { Provider } from "react-redux";
+import store from "src/store";
 
 const MyApp: React.FC<AppPropsWithLayout> = ({ Component, pageProps }) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(
-    <CookiesProvider>
-      <Component {...pageProps} />
-    </CookiesProvider>
+  return (
+    <Provider store={store}>
+      {getLayout(
+        <CookiesProvider>
+          <Component {...pageProps} />
+        </CookiesProvider>
+      )}
+    </Provider>
   );
 };
 
