b6ff379b57b0b054c5126ee1bfd4ef52778ea26b
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-10-05 16:52:00 -0300

    TODO notes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// TODO: Read baseURL from .env file
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://192.168.1.80:3001",
   timeout: 5000,
 });
 
