f78867a42364d4965e721b74b0874e5659563c04
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-10-07 09:24:48 -0300

    changed text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
-import { LandingHeader } from "@components/ZorkLandingHeader"
-import Head from "next/head"
-import styles from './styles.module.scss'
+import { LandingHeader } from "@components/ZorkLandingHeader";
+import Head from "next/head";
+import styles from "./styles.module.scss";
 
 const Landing: React.FC = () => {
-    return(
-        <>
-            <Head>
-                <title>Zork - home</title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Zork - home</title>
+      </Head>
 
-                <LandingHeader/>
-                 <main className={styles.landingContainer}>
-                     <section>
-                        <aside>
-                            <h3>Welcome to Zork</h3>
-                            <h1>
-                                Best way to <br/>
-                                make online <br/>
-                                transactions <span>.</span>
-                            </h1>
-                        </aside>
-                        <img src="/images/illustration.svg" alt="Illustration" />
-                     </section>
-                     <footer>
-                        <div>
-                            <img src="/images/shield.svg" alt="Shield"/>
-                            <p>
-                                A new way to make the transactions and <br />
-                                payments easy, reliable and secure. 
-                            </p>
-                        </div>
-                       
-                     </footer>
-                </main>
-        </>
-    )
-} 
+      <LandingHeader />
+      <main className={styles.landingContainer}>
+        <section>
+          <aside>
+            <h3>Welcome to Zork</h3>
+            <h1>
+              Best way to <br />
+              make online <br />
+              transactions <span>.</span>
+            </h1>
+          </aside>
+          <img src="/images/illustration.svg" alt="Illustration" />
+        </section>
+        <footer>
+          <div>
+            <img src="/images/shield.svg" alt="Shield" />
+            <p>
+              A new way to make transactions. <br /> Reliable and secure.
+            </p>
+          </div>
+        </footer>
+      </main>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
