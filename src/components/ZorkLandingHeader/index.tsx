803efd9b319c98a3fdc8178aaa1b3dc68745265b
Peterson Adami Candido <contact.petersonac@protonmail.com>
2021-10-07 09:15:22 -0300

    index links

--- src/components/ZorkLandingHeader/index.tsx
+++ src/components/ZorkLandingHeader/index.tsx
@@ -1,18 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
+import Link from "next/link";
 
 export const LandingHeader: React.FC = () => {
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.leftSide}>
-                <img src="/images/landing-logo.svg" alt="Zork" />
-                <h3>Developers</h3>
-                <h3>Github</h3>
-            </div>
-            <div className={styles.rightSide}>
-                <button>Sign up</button>
-                <button>Log in</button>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.leftSide}>
+        <img src="/images/landing-logo.svg" alt="Zork" />
+        <Link
+          href={{ pathname: "https://github.com/wetrustinprize/zork-frontend" }}
+        >
+          <a>
+            {" "}
+            <h3>Github / Developers</h3>
+          </a>
+        </Link>
+      </div>
+      <div className={styles.rightSide}>
+        <Link href={{ pathname: "/signup" }}>
+          <button>
+            <a>Sign up</a>
+          </button>
+        </Link>
+        <Link href={{ pathname: "/login" }}>
+          <button>
+            <a>Log in</a>
+          </button>
+        </Link>
+      </div>
+    </header>
+  );
+};
